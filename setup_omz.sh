b06e6be2212280fc06cdc639ba85c69844da3fea
chinoudom19 <chinoudom80@gmail.com>
2025-06-06 16:12:28 +0700

    Complete script install oh my zsh

---
+++ setup_omz.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ZSHRC="$HOME/.zshrc"
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+echo "🔄 Updating system and installing dependencies..."
+sudo apt update > /dev/null 2>&1
+sudo apt install -y zsh curl git > /dev/null 2>&1
+
+echo "📦 Installing Oh My Zsh..."
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" > /dev/null 2>&1
+
+echo "🔌 Installing plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions" > /dev/null 2>&1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" > /dev/null 2>&1
+
+echo "⚙️  Configuring .zshrc..."
+sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="cloud"/' "$ZSHRC"
+sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' "$ZSHRC"
+
+echo "⚡ Starting zsh..."
+exec zsh
